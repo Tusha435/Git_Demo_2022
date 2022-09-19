254c7ebcf9f786e022a8df4b2eed865e4cd5a742
Tushar Sinha <tusha435@gmail.com>
2022-09-19 16:43:04 +0530

    This is my first commit

---
+++ f1.py
@@ -0,0 +1,4 @@
+str ="This is going to be my first income"
+
+a = str.len()
+print(a)
---
+++ f2.py
@@ -0,0 +1,6 @@
+Hey Baby movie Jee lene de iss pal ko
+Principal = 100
+Rate = 10
+Time = 5
+Simple_Interest = Principal * Rate * Time
+print(Simple_Interest)
